6597d7091abd3c0615bbe23834dc99a92f832f08
Melodi Demirag <melodi@hicoral.com>
2023-05-25 22:36:41 +0300

    Put voxy picture to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import pic from './voxy.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={pic} className="App-logo" alt="logo" />
     </div>
   );
 }
